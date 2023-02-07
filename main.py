348476978c4672b12bf338a0222fcfe1066b4ac4
mark.borland <L00161836@atu.ie>
2023-02-07 11:23:37 +0000

    Added code to train and test model.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pathlib
 import tensorflow as tf
+from tensorflow.keras import models, layers
 import matplotlib.pyplot as plt
 
 # Setting up the directory path for the gathered images
@@ -9,13 +10,12 @@ imageDirectory = pathlib.Path('input')
 imageCount = len(list(imageDirectory.glob('**/*.png')))
 print(imageCount)
 
-# Define the standardised image size and batch size (amount of images the model will
-# train with at a time).
+# Define the standardised image size and batch size (amount of images the model will train with at a time).
 batchSize = 32
-imageHeight = 180
-imageWidth = 180
+imageHeight = 32
+imageWidth = 32
 
-# Creating the training dataset from 80% of the overall dataset using the parameters set above
+# Creating the training dataset from 80% of the overall dataset using the parameters set above.
 trainingDataset = tf.keras.utils.image_dataset_from_directory(
     imageDirectory,
     validation_split=0.2,
@@ -25,7 +25,7 @@ trainingDataset = tf.keras.utils.image_dataset_from_directory(
     batch_size=batchSize
 )
 
-# Creating the testing dataset from 20% of the overall dataset using the parameters set above
+# Creating the testing dataset from 20% of the overall dataset using the parameters set above.
 validationDataset = tf.keras.utils.image_dataset_from_directory(
     imageDirectory,
     validation_split=0.2,
@@ -35,17 +35,34 @@ validationDataset = tf.keras.utils.image_dataset_from_directory(
     batch_size=batchSize
 )
 
-classNames = trainingDataset.class_names
-print(classNames)
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 
-plt.figure(figsize=(10, 10))
-for images, labels in trainingDataset.take(1):
-    for i in range(9):
-        ax = plt.subplot(3, 3, i+1)
-        plt.imshow(images[i].numpy().astype("uint8"))
-        plt.title(classNames[labels[i]])
-        plt.axis("off")
+model.summary()
+
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10))
+
+model.summary()
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+history = model.fit(trainingDataset, epochs=30,
+                    validation_data=validationDataset)
+
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.5, 1])
+plt.legend(loc='lower right')
 
 plt.show()
-
 
