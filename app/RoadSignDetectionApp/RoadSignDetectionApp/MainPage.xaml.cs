427d9bbb2d9cd30132360ed972b50a812e0399fe
Mark Borland <L00161836@student.lyit.ie>
2023-02-17 10:20:03 +0000

    Initial design.

--- app/RoadSignDetectionApp/RoadSignDetectionApp/MainPage.xaml.cs
+++ app/RoadSignDetectionApp/RoadSignDetectionApp/MainPage.xaml.cs
@@ -4,22 +4,11 @@ namespace RoadSignDetectionApp;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	public async FileResult TakePhoto()
-	{
-		if (MediaPicker.Default.IsCaptureSupported)
-		{
-			FileResult photo = await MediaPicker.Default.CapturePhotoAsync();
-
-			return photo;
-
-		}
-	}
 }
 
