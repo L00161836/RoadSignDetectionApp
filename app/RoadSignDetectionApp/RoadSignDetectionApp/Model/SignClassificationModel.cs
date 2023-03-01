b9045a1ff9a478fbc113c27a3be21fc00ed1cd08
Mark Borland <L00161836@student.lyit.ie>
2023-03-01 13:34:24 +0000

    Integrated model.

---
+++ app/RoadSignDetectionApp/RoadSignDetectionApp/Model/SignClassificationModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoadSignDetectionApp.Model
+{
+    public class SignClassificationModel
+    {
+        public string SignName { get; }
+        public float Probability { get; }
+        public SignClassificationModel(string signName, float probability)
+        {
+            SignName = signName;
+            Probability= probability;
+        }
+    }
+}
