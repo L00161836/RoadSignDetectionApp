7e7eecce78dde119705adfa4cc1f6963c0539670
Mark Borland <L00161836@student.lyit.ie>
2023-03-08 12:21:45 +0000

    Updated model.

--- model/main.py
+++ model/main.py
@@ -1,84 +1,74 @@
 import pathlib
-# import numpy as np
-# import matplotlib.pyplot as plt
-# import os
+import numpy as np
+import matplotlib.pyplot as plt
+import os
 import tensorflow as tf
-# import tensorflow_datasets as tfds
-# from keras import backend as K
-#
-# # Setting up the directory path for the gathered images
-# imageDirectory = pathlib.Path('input')
-#
-# # Displays the amount of images in the dataset
-# imageCount = len(list(imageDirectory.glob('**/*.png')))
-# print(imageCount)
-#
-# # Define the standardised image size and batch size (amount of images the model will train with at a time).
-# batchSize = 32
-# imageSize = (180, 180)
-#
-# # Creating the training dataset from 80% of the overall dataset using the parameters set above.
-# trainingDataset = tf.keras.utils.image_dataset_from_directory(
-#     imageDirectory,
-#     validation_split=0.2,
-#     subset="training",
-#     seed=123,
-#     image_size=imageSize,
-#     batch_size=batchSize
-# )
-#
-# # Creating the testing dataset from 20% of the overall dataset using the parameters set above.
-# validationDataset = tf.keras.utils.image_dataset_from_directory(
-#     imageDirectory,
-#     validation_split=0.2,
-#     subset="validation",
-#     seed=123,
-#     image_size=imageSize,
-#     batch_size=batchSize
-# )
-#
-# classNames = trainingDataset.class_names
-#
-# validationBatches = tf.data.experimental.cardinality(validationDataset)
-# testDataset = validationDataset.take(validationBatches // 5)
-# validationDataset = validationDataset.skip(validationBatches // 5)
-#
-# autotune = tf.data.AUTOTUNE
-#
-# trainingDataset = trainingDataset.cache().prefetch(buffer_size=autotune)
-# validationDataset = validationDataset.cache().prefetch(buffer_size=autotune)
-# testDataset = testDataset.cache().prefetch(buffer_size=autotune)
-#
+import tensorflow_datasets as tfds
+from keras import backend as K
+
+# Setting up the directory path for the gathered images
+imageDirectory = pathlib.Path('input')
+
+# Displays the amount of images in the dataset
+imageCount = len(list(imageDirectory.glob('**/*.png')))
+print(imageCount)
+
+# Define the standardised image size and batch size (amount of images the model will train with at a time).
+batchSize = 32
+imageSize = (180, 180)
+
+# Creating the training dataset from 80% of the overall dataset using the parameters set above.
+trainingDataset = tf.keras.utils.image_dataset_from_directory(
+    imageDirectory,
+    validation_split=0.2,
+    subset="training",
+    seed=123,
+    image_size=imageSize,
+    batch_size=batchSize
+)
+
+# Creating the testing dataset from 20% of the overall dataset using the parameters set above.
+validationDataset = tf.keras.utils.image_dataset_from_directory(
+    imageDirectory,
+    validation_split=0.2,
+    subset="validation",
+    seed=123,
+    image_size=imageSize,
+    batch_size=batchSize
+)
+
+classNames = trainingDataset.class_names
+
+validationBatches = tf.data.experimental.cardinality(validationDataset)
+testDataset = validationDataset.take(validationBatches // 5)
+validationDataset = validationDataset.skip(validationBatches // 5)
+
+autotune = tf.data.AUTOTUNE
+
+trainingDataset = trainingDataset.cache().prefetch(buffer_size=autotune)
+validationDataset = validationDataset.cache().prefetch(buffer_size=autotune)
+testDataset = testDataset.cache().prefetch(buffer_size=autotune)
+
 # dataAugmentation = tf.keras.Sequential([
 #     tf.keras.layers.RandomFlip('horizontal_and_vertical'),
 #     tf.keras.layers.RandomRotation(0.3),
 #     tf.keras.layers.RandomBrightness(0.5),
 #     tf.keras.layers.RandomContrast(0.5)
 # ])
-#
+# #
 # preprocessInput = tf.keras.applications.mobilenet_v2.preprocess_input
-# rescale = tf.keras.layers.Rescaling(1./127.5, offset=-1)
+# rescale = tf.keras.layers.Rescaling(1./255, offset=-1)
 #
-# imageShape = imageSize + (3,)
-# baseModel = tf.keras.applications.MobileNetV2(input_shape=imageShape,
+# imageShape = (180, 180, 3)
+# baseModel = tf.keras.applications.MobileNetV2(input_shape=(180, 180, 3),
 #                                               include_top=False,
 #                                               weights='imagenet')
 #
-# imageBatch, labelBatch = next(iter(trainingDataset))
-# featureBatch = baseModel(imageBatch)
-# print(featureBatch.shape)
-#
-# baseModel.trainable = False
-#
-# baseModel.summary()
+# baseModel.trainable = True
 #
 # globalAverageLayer = tf.keras.layers.GlobalAveragePooling2D()
-# featureBatchAverage = globalAverageLayer(featureBatch)
-# print(featureBatchAverage.shape)
 #
 # predictionLayer = tf.keras.layers.Dense(3, activation='relu')
-# predictionBatch = predictionLayer(featureBatchAverage)
-# print(predictionBatch.shape)
 #
 # inputs = tf.keras.Input(shape=(180, 180, 3))
 # x = dataAugmentation(inputs)
@@ -95,6 +85,40 @@ import tensorflow as tf
 #               metrics=['accuracy'])
 #
 # model.summary()
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.experimental.preprocessing.Rescaling(1. / 255, input_shape=(180, 180, 3)),
+    tf.keras.layers.RandomFlip('horizontal_and_vertical'),
+    tf.keras.layers.RandomRotation(0.3),
+    tf.keras.layers.RandomBrightness(0.5),
+    tf.keras.layers.RandomContrast(0.5),
+    tf.keras.applications.MobileNetV2(input_shape=(180, 180, 3),
+                                      include_top=False,
+                                      weights='imagenet'),
+    tf.keras.layers.Conv2D(128, 3, padding='same'),
+    tf.keras.layers.LeakyReLU(alpha=0.1),
+    tf.keras.layers.Conv2D(64, 3, padding='same'),
+    tf.keras.layers.LeakyReLU(alpha=0.1),
+    tf.keras.layers.Conv2D(32, 3, padding='same'),
+    tf.keras.layers.LeakyReLU(alpha=0.1),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(16, activation='relu'),
+])
+
+model.add(tf.keras.layers.Dense(units=3, activation='softmax'))
+
+model.summary()
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(
+    trainingDataset,
+    validation_data=validationDataset,
+    epochs=12
+)
+
 #
 # print(len(model.trainable_variables))
 #
@@ -139,7 +163,7 @@ import tensorflow as tf
 #
 # loss += historyFine.history['loss']
 # val_loss += historyFine.history['val_loss']
-#
+# #
 # plt.figure(figsize=(8, 8))
 # plt.subplot(2, 1, 1)
 # plt.plot(acc, label='Training Accuracy')
@@ -156,12 +180,18 @@ import tensorflow as tf
 # plt.title('Training and Validation Loss')
 # plt.xlabel('epoch')
 # plt.show()
-#
+
 # model.save("finished_model")
 
-savedModelDirectory = pathlib.Path('finished_model')
-converter = tf.lite.TFLiteConverter.from_saved_model('finished_model')
-tflite_model = converter.convert()
+# savedModelDirectory = pathlib.Path('finished_model')
+# converter = tf.lite.TFLiteConverter.from_saved_model('finished_model')
+# tflite_model = converter.convert()
+#
+# with open("model.tflite", "wb") as f:
+#     f.write(tflite_model)
+
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tfliteModel = converter.convert()
 
 with open("model.tflite", "wb") as f:
-    f.write(tflite_model)
+    f.write(tfliteModel)
